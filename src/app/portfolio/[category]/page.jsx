61bddf910c942aa96915b0023c408b8cbd75b421
Abu Ahad <abuahad35188@gmail.com>
2025-08-02 11:24:42 -0700

    protfolio and  category page set up

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,11 +1,31 @@
+ import Button from '@/Components/Button/Button';
+import Image from 'next/image';
 import React from 'react';
-
-const CategoryPage = () => {
+import style from './page.module.css'
+ 
+ const Category = ({params}) => {
+    console.log(params)
     return (
-        <div>
-            category
+      <div className={style.container}>
+        <h1 className={style.catTile}>{params.category}</h1>
+        <div className={style.item}>
+          <div className={style.content}>
+            <h1 className={style.test}>test</h1>
+            <p className={style.desc}>desc</p>
+            <Button text="/See More" url="#" />
+          </div>
+          <div className={style.imageContainer}>
+            <Image
+              className={style.img}
+              src="/office.jpg"
+              alt="Modern office interior with natural light"
+              width={800}
+              height={600}
+            />
+          </div>
         </div>
+      </div>
     );
-};
-
-export default CategoryPage;
+ };
+ 
+ export default Category;
