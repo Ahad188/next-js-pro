6b03b9207f8020afabc7c7017b76f01793ffdc6a
Abu Ahad <abuahad35188@gmail.com>
2025-08-08 11:08:04 -0700

    fech data for blog post

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,46 +1,39 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
-import styles from './page.module.css'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import styles from "./page.module.css";
 
-const BlogPage = () => {
-    return (
-        <div className={styles.mainContainer}>
-            <Link href='/blog/id' className={styles.container}>
-                <div className={styles.imgContainer}>
-                    <Image
-                    src='/illustration.png'
-                    alt=''
-                    width={400}
-                    height={250}
-                    className={styles.imag}
-                    ></Image>
-                </div>
-                <div className={styles.contain}>
-                    <h1 className={styles.title}>test uouasioldjhi</h1>
-                    <p className={styles.desc}>d dec <br /> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat magnam facere consequatur possimus quisquam aliquam?</p>
-                    <p></p>
-                </div>
-            </Link>
-            <Link href='/blog/id' className={styles.container}>
-                <div className={styles.imgContainer}>
-                    <Image
-                    src='/illustration.png'
-                    alt=''
-                    width={400}
-                    height={250}
-                    className={styles.imag}
-                    ></Image>
-                </div>
-                <div className={styles.contain}>
-                    <h1 className={styles.title}>test uouasioldjhi</h1>
-                    <p className={styles.desc}>d dec <br /> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat magnam facere consequatur possimus quisquam aliquam?</p>
-                    <p></p>
-                </div>
-            </Link>
-            
-        </div>
-    );
-};
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  if (!res.ok) throw new Error("Failed to fetch data");
+  return res.json();
+}
 
-export default BlogPage;
+export default async function BlogPage() {
+  const data = await getData();
+  return (
+    <div className={styles.mainContainer}>
+      {data.map((item) => (
+        <Link
+          href={`/blog/${item.id}`}
+          className={styles.container}
+          key={item.id}
+        >
+          <div className={styles.imageContainer}>
+            <Image
+              src="/illustration.png" // Fallback since JSONPlaceholder doesn't provide images
+              alt=""
+              width={400}
+              height={250}
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.body}</p>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+}
